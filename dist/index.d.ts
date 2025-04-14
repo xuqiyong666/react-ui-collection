3b84124a096d3b08139cc0fae5f7ea1822b24319
xuqiyong666 <xuqiyong666@163.com>
2025-04-14 19:42:41 +0800

    build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,3 @@
+export { default as SpringNumber } from "./components/SpringNumber/SpringNumber";
 export { default as AnimatedTitle } from "./components/AnimatedTitle/AnimatedTitle";
 export { default as Button } from "./components/Button/Button";
